1ed514c46771287c5bf686091fd4e940397f2e42
Ferran Campos <ferrancl@kth.se>
2025-03-31 17:53:27 +0200

    feat: header & timeline

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from 'src/app/components/header/header.component';
+import { TimelineComponent } from '../../components/timeline/timeline.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, TimelineComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
