e1b983d32b96865ab4fc92ec2db3dcd8e2b5f8b9
Ferran Campos <ferrancl@kth.se>
2025-04-03 14:57:53 +0200

    feat: draggable circle in timeline

--- src/app/components/timeline/timeline.component.ts
+++ src/app/components/timeline/timeline.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { TyrAlert } from 'tyr-js';
 
 @Component({
@@ -8,13 +8,44 @@ import { TyrAlert } from 'tyr-js';
 })
 export class TimelineComponent {
   public alerts: TyrAlert[];
+  @ViewChild('scrollContainer') private scrollContainer!: ElementRef;
+  @ViewChild('slideCircle') private slideCircle!: ElementRef;
 
-  constructor() {
+  private isMouseClicked: Boolean;
+
+  constructor(private renderer: Renderer2) {
     this.alerts = [];
+    this.isMouseClicked = false;
+  }
+
+  ngAfterViewInit() {
+    const element = this.slideCircle.nativeElement;
+
+    this.renderer.listen(
+      element,
+      'mousedown',
+      (event: MouseEvent) => (this.isMouseClicked = true)
+    );
+    this.renderer.listen(document, 'mousemove', (event: MouseEvent) => {
+      if (!this.isMouseClicked) return;
+      const offsetX = event.clientX;
+      this.renderer.setStyle(
+        this.slideCircle.nativeElement,
+        'transform',
+        `translateX(${offsetX}px)`
+      );
+    });
+    this.renderer.listen(
+      document,
+      'mouseup',
+      () => (this.isMouseClicked = false)
+    );
   }
 
   public addAlert(alert: TyrAlert) {
     this.alerts.push(alert);
+    const container = this.scrollContainer.nativeElement;
+    container.scrollLeft = container.scrollWidth;
   }
 
   public deleteAlert(alert: TyrAlert) {
