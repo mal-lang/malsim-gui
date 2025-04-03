f4357708f51e135c733daeef769120cb227c1173
Ferran Campos <ferrancl@kth.se>
2025-04-03 16:18:58 +0200

    feat: slide to element on drop

--- src/app/components/timeline/timeline.component.ts
+++ src/app/components/timeline/timeline.component.ts
@@ -8,14 +8,12 @@ import { TyrAlert } from 'tyr-js';
 })
 export class TimelineComponent {
   public alerts: TyrAlert[];
-  @ViewChild('scrollContainer') private scrollContainer!: ElementRef;
   @ViewChild('slideCircle') private slideCircle!: ElementRef;
-  @ViewChild('timelineList') private timelineList!: ElementRef;
+  @ViewChild('slideLine') private slideLine!: ElementRef;
 
   private isMouseClicked: Boolean;
   private draggableLeftLimit: number;
   private draggableRightLimit: number;
-  private hasBeenChecked: Boolean = false;
 
   constructor(private renderer: Renderer2) {
     this.alerts = [];
@@ -35,35 +33,72 @@ export class TimelineComponent {
       'transform',
       `translateX(${startPosition}px)`
     );
+    this.updateLineColor(startPosition, element.getBoundingClientRect().width);
 
     this.renderer.listen(
       element,
       'mousedown',
-      (event: MouseEvent) => (this.isMouseClicked = true)
+      () => (this.isMouseClicked = true)
     );
     this.renderer.listen(document, 'mousemove', (event: MouseEvent) => {
       if (!this.isMouseClicked) return;
       const offsetX = event.clientX;
       if (offsetX > this.draggableRightLimit) return;
       if (offsetX <= this.draggableLeftLimit) return;
+      this.updateLineColor(offsetX, element.getBoundingClientRect().width);
       this.renderer.setStyle(
         this.slideCircle.nativeElement,
         'transform',
         `translateX(${offsetX}px)`
       );
     });
-    this.renderer.listen(
-      document,
-      'mouseup',
-      () => (this.isMouseClicked = false)
+    this.renderer.listen(document, 'mouseup', (event: MouseEvent) => {
+      this.isMouseClicked = false;
+      const offsetX = event.clientX;
+      const element = this.slideCircle.nativeElement;
+      const position = this.calculateSelectedItem(offsetX);
+      this.renderer.setStyle(
+        this.slideCircle.nativeElement,
+        'transform',
+        `translateX(${position}px)`
+      );
+      this.updateLineColor(position, element.getBoundingClientRect().width);
+    });
+  }
+
+  calculateSelectedItem(position: number) {
+    if (position < 24) return 0;
+
+    let aux = 24;
+    for (let i = 0; aux < position; i++) {
+      aux += 136;
+    }
+
+    return aux - 88;
+  }
+  updateLineColor(position: number, width: number) {
+    const line = this.slideLine.nativeElement as HTMLElement;
+
+    this.renderer.setStyle(
+      line,
+      'background',
+      `linear-gradient(to right, #ffa100 ${position + width / 2}px, #343a3e ${
+        position + width / 2
+      }px)`
     );
+  }
+
+  updateLineWidth() {
+    const line = this.slideLine.nativeElement as HTMLElement;
+    this.renderer.setStyle(line, 'width', `${this.draggableRightLimit}px`);
   }
 
   public addAlert(alert: TyrAlert) {
     if (this.alerts.length > 0)
-      this.draggableRightLimit += 136; //128px of item width + 8px of gap
-    else this.draggableRightLimit += 72;
+      this.draggableRightLimit += 136; //128px of item width + 8px of gap +
+    else this.draggableRightLimit += 80;
     this.alerts.push(alert);
+    this.updateLineWidth();
   }
 
   public deleteAlert(alert: TyrAlert) {
