4ef6c7135f2a34b124ecbca341e43656f947adab
Ferran Campos <ferrancl@kth.se>
2025-04-08 13:54:18 +0200

    feat: asset menu alerts

---
+++ src/app/components/asset-menu-alerts/asset-menu-alerts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-asset-menu-alerts',
+  standalone: true,
+  imports: [],
+  templateUrl: './asset-menu-alerts.component.html',
+  styleUrl: './asset-menu-alerts.component.scss'
+})
+export class AssetMenuAlertsComponent {
+
+}
--- src/app/components/asset-menu/asset-menu.component.ts
+++ src/app/components/asset-menu/asset-menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AssetMenuInformationComponent } from '../asset-menu-information/asset-menu-information.component';
+import { AssetMenuAlertsComponent } from '../asset-menu-alerts/asset-menu-alerts.component';
 
 @Component({
   selector: 'app-asset-menu',
   standalone: true,
-  imports: [AssetMenuInformationComponent],
+  imports: [AssetMenuInformationComponent, AssetMenuAlertsComponent],
   templateUrl: './asset-menu.component.html',
   styleUrl: './asset-menu.component.scss',
 })
