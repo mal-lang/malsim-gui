0f14fc96ad00c3194675a1058d1a6c3a4e198baa
Ferran Campos <ferrancl@kth.se>
2025-04-08 15:05:44 +0200

    feat: hover background color change

--- src/app/components/asset-menu/asset-menu.component.ts
+++ src/app/components/asset-menu/asset-menu.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-import { AssetMenuInformationComponent } from '../asset-menu-information/asset-menu-information.component';
 import { AssetMenuAlertsComponent } from '../asset-menu-alerts/asset-menu-alerts.component';
+import { NgClass, NgIf } from '@angular/common';
+import { AssetMenuInformationComponent } from '../asset-menu-information/asset-menu-information.component';
 
 @Component({
   selector: 'app-asset-menu',
   standalone: true,
-  imports: [AssetMenuInformationComponent, AssetMenuAlertsComponent],
+  imports: [
+    NgIf,
+    NgClass,
+    AssetMenuAlertsComponent,
+    AssetMenuInformationComponent,
+  ],
   templateUrl: './asset-menu.component.html',
   styleUrl: './asset-menu.component.scss',
 })
-export class AssetMenuComponent {}
+export class AssetMenuComponent {
+  public openedMenu: string = 'information';
+
+  public openMenu(menu: string) {
+    this.openedMenu = menu;
+  }
+}
